9c7ed7e9b491ed151f7300dccabfd442997f2591
SMugallimov <smugallimov@mail.ru>
2022-11-23 22:04:32 +0400

    Stepik Python course 2. Input-Output Data

---
+++ inputOutput/printInputCommands.py
@@ -0,0 +1,40 @@
+print("Hello, world!")
+
+print("4", "8", "15", "16", "23", "42")
+
+print("4")
+print("8")
+print("15")
+print("16")
+print("23")
+print("42")
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
+
+name = input()
+print("Hello,", name)
+
+team = input()
+print(team, "- champion!")
+
+string1 = input()
+string2 = input()
+string3 = input()
+
+print(string1)
+print(string2)
+print(string3)
+
+string1 = input()
+string2 = input()
+string3 = input()
+
+print(string3)
+print(string2)
+print(string1)
