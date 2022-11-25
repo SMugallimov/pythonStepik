3091dc6669dd66cd12f7cf6dded3b747a23d95db
SMugallimov <smugallimov@mail.ru>
2022-11-25 16:56:14 +0400

    Stepik Python course 2 Input-Output Data 2.3 sep and end Parameters

---
+++ inputOutput/paramSepEnd.py
@@ -0,0 +1,11 @@
+print('I', 'like', 'Python', sep='***')
+
+sep = input()
+string1 = input()
+string2 = input()
+string3 = input()
+
+print(string1, string2, string3, sep=sep)
+
+name = input()
+print("Hello,", name, end='!')
